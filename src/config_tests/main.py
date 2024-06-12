aebe85378960fb8dfd13eefea3e557f36f5dacf3
dpuzio <dominika.makuch.mail@gmail.com>
2024-06-12 17:08:57 +0200

    Hocon files as a source of configuration for Spark on Databricks

--- src/config_tests/main.py
+++ src/config_tests/main.py
@@ -1,12 +1,33 @@
 from pyspark.sql import SparkSession
+import argparse
+from pyhocon import ConfigFactory
 
-
-def get_count(table_identifier: str, spark_session: SparkSession):
-  return spark_session.read.table(table_identifier).count()
+# def get_count(table_identifier: str, spark_session: SparkSession):
+#     return spark_session.read.table(table_identifier).count()
 
 
 def main():
-  print("Hello!")
+    # TODO:
+    # 2. use hocon to read conf files
+    # 3. test it we can get parameters in the spark job
+    # 4. showcase how to parametrize SQL statement
+    # 5. suggest a better hocon files organization (e.g. with nested structures)
+    # 6. showcase local unit testing
+
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--config_file_path", type=str, required=True)
+
+    args = parser.parse_args()
+    config_file_path = args.config_file_path
+    print(f'Reading configuration from {config_file_path}')
+
+    config = ConfigFactory.parse_file(f'/Workspace/{config_file_path}')
+    schema_name = config.get('schema_name')
+    print(f'Parsed out schema name: {schema_name}')
+
+    spark_session = SparkSession.builder.getOrCreate()
+
+
 
 if __name__ == "__main__":
-  main()
+    main()
